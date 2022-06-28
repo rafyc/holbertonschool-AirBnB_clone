675e3e2213daf7277bd79eed813d12e9bb1c2f32
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-06-28 11:54:13 +0200

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -13,13 +13,16 @@ class BaseModel:
     The BaseModel Class
     """
 
-    def __init__(self):
-        """
-        Function that init an instance of class BaseModel with uniq ID
-        """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) != 0:
+            for keys, values in kwargs.items():
+                if keys in ['created_at', 'updated_at']:
+                    values = datetime.strptime(values, "%Y-%m-%dT%H:%M:%S.%f")
+                if keys != '__class__':
+                    setattr(self, keys, values)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
 
     def __str__(self):
         """
@@ -38,9 +41,11 @@ class BaseModel:
         """returns a dictionary containing all keys/values of __dict__
         of the instance
         """
-        instance_dict = self.__dict__.copy()
-        instance_dict["__class__"] = self.__class__.__name__
-        instance_dict["created_at"] = self.created_at.isoformat()
-        instance_dict["updated_at"] = self.updated_at.isoformat()
 
+        instance_dict = {"__class__": self.__class__.__name__}
+        for keys, values in self.__dict__.items():
+            if keys in ['created_at', 'updated_at']:
+                instance_dict[keys] = values.isoformat()
+            else:
+                instance_dict[keys] = values
         return instance_dict
