913a15da68e444b54a26bc13e92aa037598f65eb
comels <come.l.s@hotmail.fr>
2022-07-02 07:49:54 +0000

    user

--- models/user.py
+++ models/user.py
@@ -8,6 +8,12 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """ The User Class
+    
+    Attributes:
+        - email (str)
+        - password (str)
+        - first_name (str)
+        - last_name (str)
     """
     email = ""
     password = ""
